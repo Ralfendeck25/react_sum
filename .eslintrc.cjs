0b50d21320368f1372f517079ec83c90e6088b79
Ralfendeck25 <poseidoncenghizerzhemetmhossad@outlook.com>
2025-05-01 06:40:45 -0300

    MakeItRain

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -37,7 +37,6 @@ module.exports = {
         SwitchCase: 1,
         VariableDeclarator: 1,
         outerIIFEBody: 1,
-        // MemberExpression: null,
         FunctionDeclaration: {
           parameters: 1,
           body: 1,
@@ -53,7 +52,6 @@ module.exports = {
         ObjectExpression: 1,
         ImportDeclaration: 1,
         flatTernaryExpressions: false,
-        // list derived from https://github.com/benjamn/ast-types/blob/HEAD/def/jsx.js
         ignoredNodes: [
           'JSXElement',
           'JSXElement > *',
--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,4 +1,23 @@
 module.exports = {
-  extends: "@mate-academy/stylelint-config",
-  rules: {}
+  extends: '@mate-academy/stylelint-config',
+  rules: {
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: [
+          'if',
+          'else',
+          'for',
+          'each',
+          'while',
+          'mixin',
+          'include',
+          'extend',
+          'function',
+          'return',
+        ],
+      },
+    ],
+    'no-descending-specificity': null,
+  },
 };
